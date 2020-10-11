7eb992e393a0ac024fb118f1324d600d9043c5c7
Keven Iskander <keven.iskander@hotmail.com>
2020-10-11 13:23:56 -0400

    Part 2, Task 4 complete

--- app/src/main/java/com/example/androidassignments/LoginActivity.java
+++ app/src/main/java/com/example/androidassignments/LoginActivity.java
@@ -2,20 +2,38 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     protected static final String LoginActivity = "LoginActivity";
-    private Button login_button;
+
+    private Object DefaultEmail;
+    private Button BtnLogin;
+    private EditText EmailAddress;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Log.i(LoginActivity, "In onCreate()");
+
+        SharedPreferences shared = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit=shared.edit();
+        edit.putString("DefaultEmail", "email@domain.com");
+        edit.commit();
+
+
     }
     public void onResume(){
         super.onResume();
@@ -42,5 +60,16 @@ public class LoginActivity extends AppCompatActivity {
     public void onDestroy(){
         super.onDestroy();
         Log.i(LoginActivity, "In onDestroy()");
+    }
+
+    public void saveEmail(View view){
+        SharedPreferences shared = getSharedPreferences("Login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit=shared.edit();
+        EditText text = (EditText)findViewById(R.id.login);
+        edit.putString("userEmail", text.getText().toString());
+        edit.commit();
+
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
