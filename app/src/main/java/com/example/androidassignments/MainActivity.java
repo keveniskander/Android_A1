83bdfc870d92d1f02bf548355069c88b1186eca0
Keven Iskander <keven.iskander@hotmail.com>
2020-10-11 14:00:55 -0400

    Part 2, Task 6 complete

--- app/src/main/java/com/example/androidassignments/MainActivity.java
+++ app/src/main/java/com/example/androidassignments/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.androidassignments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(MainActivity, "In onCreate()");
+
+        Button button = (Button)findViewById(R.id.button);
     }
 
     public void onResume(){
@@ -46,7 +51,19 @@ public class MainActivity extends AppCompatActivity {
         Log.i(MainActivity, "In onDestroy()");
     }
 
+    public void startActivityForResult(View view) {
 
 
+        Intent intent = new Intent(MainActivity.this, ListItemsActive.class);
+        startActivityForResult(intent, 10);
+
+    }
+
+    public void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+        if (requestCode == 10) {
+            Log.i(MainActivity, "Returned to MainActivity.onActivityResult");
+        }
+    }
 
 }
