783eda275be325d1c3429b1a6507e0ade2e8d4e5
Keven Iskander <keven.iskander@hotmail.com>
2020-10-11 15:02:05 -0400

    Part 2, Task 7 complete

--- app/src/main/java/com/example/androidassignments/ListItemsActive.java
+++ app/src/main/java/com/example/androidassignments/ListItemsActive.java
@@ -3,14 +3,22 @@ package com.example.androidassignments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.LauncherActivity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import java.util.List;
 
 public class ListItemsActive extends AppCompatActivity {
 
     protected static final String ListItemsActive = "ListItemsActive";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +50,25 @@ public class ListItemsActive extends AppCompatActivity {
     public void onDestroy(){
         super.onDestroy();
         Log.i(ListItemsActive, "In onDestroy()");
+    }
+
+    public void onClickImage(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e){
+            // display error state to user
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ImageButton imageButton = (ImageButton)findViewById(R.id.image_button);
+            imageButton.setImageBitmap(imageBitmap);
+        }
     }
 }
