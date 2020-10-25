d42d9a4f94c29883fb7ce14bfa373204ea3d471a
Keven Iskander <keven.iskander@hotmail.com>
2020-10-25 17:27:52 -0400

    Part 3 Task 4 Complete

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -3,12 +3,33 @@ package com.example.androidassignments;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
+
+    private ListView chatList;
+    private EditText chatText;
+    private Button chatButton;
+    private ArrayList<String> chatMessage = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
+        ListView chatList = (ListView) findViewById(R.id.chatList);
+        final EditText chatText = (EditText) findViewById(R.id.sendText);
+        Button chatButton = (Button) findViewById(R.id.sendButton);
+
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String chatString = chatText.getText().toString();
+                chatMessage.add(chatString);
+            }
+        });
     }
 }
