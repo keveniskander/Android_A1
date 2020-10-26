09779e444e85593b35c0075e24d173f52f397287
keveniskander <keven.iskander@hotmail.com>
2020-10-25 22:32:12 -0400

    A2 Part 3 Complete

--- app/src/main/java/com/example/androidassignments/ChatWindow.java
+++ app/src/main/java/com/example/androidassignments/ChatWindow.java
@@ -5,84 +5,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Button;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ChatWindow extends AppCompatActivity {
-
-    private ListView chatList;
-    private EditText chatText;
-    private Button chatButton;
-    private ArrayList<String> chatMessage = new ArrayList<String>();
+    public ArrayList<String> messages= new ArrayList<>();
+    public EditText message;
+    public ListView chat;
+    public Button sendButton;
     public ChatAdapter messageAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
-        ListView chatList = (ListView) findViewById(R.id.chatList);
-        final EditText chatText = (EditText) findViewById(R.id.sendText);
-        Button chatButton = (Button) findViewById(R.id.sendButton);
+        chat= findViewById(R.id.chatView);
 
-        //in this case, “this” is the ChatWindow, which is-A Context object
-        messageAdapter =new ChatAdapter( this );
-        chatList.setAdapter (messageAdapter);
+        message= findViewById(R.id.sendText);
+        sendButton =findViewById(R.id.sendButton);
+
+        messageAdapter= new ChatAdapter(this);
+        chat.setAdapter(messageAdapter);
 
 
-
-//        chatButton.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                String chatString = chatText.getText().toString();
-//                chatMessage.add(chatString);
-//
-//            }
-//        });
     }
 
-    public void saveMessage(View view){
+    public void sendMessage(View view){
 
-        chatMessage.add(chatText.getText().toString());
+        messages.add(message.getText().toString());
         messageAdapter.notifyDataSetChanged();
 
-        chatText.setText("");
+        message.setText("");
 
     }
+    private class ChatAdapter extends ArrayAdapter<String>{
 
 
-    private class ChatAdapter extends ArrayAdapter<String> {
-
-
-        public ChatAdapter(Context ctx) {
-            super(ctx, 0);
+        public ChatAdapter(Context ctx){
+            super(ctx,0);
         }
+
         public int getCount(){
-            return chatMessage.size();
+
+            return messages.size();
         }
+
         public String getItem(int position){
-            return chatMessage.get(position);
+            return messages.get(position);
         }
-        public View getView(int position, View convertView, ViewGroup parent){
-            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
-            View result = null;
-            if(position %2 == 0){
-                result = inflater.inflate(R.layout.chat_row_incoming, null);
+
+        public View getView (int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater= ChatWindow.this.getLayoutInflater();
+            View result=null;
+            if (position%2==0){
+                result=inflater.inflate(R.layout.chat_row_incoming,null);
+
             }else{
-                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+                result=inflater.inflate(R.layout.chat_row_outgoing,null);
+
             }
-            TextView message = (TextView)result.findViewById(R.id.sendText);
-            message.setText(   getItem(position)  ); // get the string at position
+            TextView message=(TextView)result.findViewById(R.id.message_text);
+            message.setText(getItem(position));
+
             return result;
 
         }
-
-
     }
 
 }
+
